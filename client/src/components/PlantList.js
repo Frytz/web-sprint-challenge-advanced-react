f400b8db8805c2ecf330a1806cc6018216eae394
John Rossi <DACOOK1983@gamil.com>
2020-05-15 21:29:39 -0400

    checkout form completed

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,11 +3,24 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  constructor(list) {
+    super(list);
+    this.state = {
+      plants:[],
+    }
+  }
   // when the component mounts:
+componentDidMount(){
   //   - fetch data from the server endpoint - http://localhost:3333/plants
+axios
+  .get('http://localhost:3333/plants')
+  .then( res => {
+    console.log (res)
   //   - set the returned plants array to this.state.plants
-
+  this.setState({plants: res.data.plantsData});
+  })
+  .catch(err => console.log(err));
+}
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
